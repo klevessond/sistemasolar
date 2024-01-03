724361feaacb6e3bb22c419bf99b2118f6238239
klevesson <klevessond@gmail.com>
2024-01-03 18:23:22 -0300

    ajustado as paginas de cliente com o nome template, criado o novo botão de clientes e criado a pasta shared

--- clientes/urls.py
+++ clientes/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from clientes.views import clientes
 
 urlpatterns = [
-        path('clientes', clientes)
+        path('clientes', clientes, name='clientes')
 ]
--- clientes/views.py
+++ clientes/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def clientes(request):
-        return HttpResponse('<h1>tela de clientes</h1>')
+        return render(request, 'clientes/clientes.html')
