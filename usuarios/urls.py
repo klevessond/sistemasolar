5b0621cdbb8de1b7a62804db341755f7c06f75c9
klevessond <klevessond@gmail.com>
2024-06-18 12:48:50 -0300

    tela de login ajustada

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
-from usuarios.views import index
+from usuarios.views import index,login,cadastro,logout
 
 
 urlpatterns = [
     path('index/', index, name='index'),
+    path('login/', login, name='login'),
+    path('cadastro/', cadastro, name='cadastro'),
+    path('logout', logout, name='logout'),
+
 
 ]
--- usuarios/views.py
+++ usuarios/views.py
@@ -1,5 +1,65 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
+from usuarios.forms import LoginForms, CadastroForms
+from django.contrib.auth.models import User
+from django.contrib import auth
+
 # Create your views here.
 def index(request):
         return render(request, 'usuarios/index.html')
+
+def login(request):
+    form = LoginForms()
+
+    if request.method == 'POST':
+        form = LoginForms(request.POST)
+
+        if form.is_valid():
+            nome = form['nome_login'].value()
+            senha = form['senha'].value()
+
+        usuario = auth.authenticate(
+            request,
+            username=nome,
+            password=senha
+        )
+        if usuario is not None:
+            auth.login(request, usuario)
+            print('valido')
+            return redirect('index')
+        else:
+            print('nao valido')
+            return redirect('login')
+
+    return render(request, 'usuarios/login.html', {'form': form})
+
+def cadastro(request):
+    form = CadastroForms()
+    if request.method == 'POST':
+        form = CadastroForms(request.POST)
+
+        if form.is_valid():
+            if form["senha_1"].value() != form["senha_2"].value():
+                return redirect('cadastro')
+
+            nome=form['nome_cadastro'].value()
+            email=form['email'].value()
+            senha=form['senha_1'].value()
+
+            if User.objects.filter(username=nome).exists():
+                return redirect('cadastro')
+
+            usuario = User.objects.create_user(
+                username=nome,
+                email=email,
+                password=senha
+            )
+            usuario.save()
+            return redirect('login')
+
+    return render(request, 'usuarios/cadastro.html', {'form': form})
+
+def logout(request):
+    auth.logout(request)
+    return redirect('login')
+   
